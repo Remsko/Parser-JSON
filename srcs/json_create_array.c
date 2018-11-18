d530cdbdda10fe286b3865c9c1cb62788db0d971
Remsko <pinoit.remi@gmail.com>
2018-11-18 21:08:33 +0100

    refact: remove printf

--- srcs/json_create_array.c
+++ srcs/json_create_array.c
@@ -6,7 +6,7 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 15:35:54 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/11/18 20:22:11 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/11/18 21:07:20 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,26 @@
 
 uint32_t json_array_len(t_json_content *data)
 {
-    uint32_t len;
-    uint32_t index;
-    uint32_t *checker;
+    int32_t     *brackets;
+    uint32_t    index;
+    uint32_t    len;
+    bool        quote;
 
-    checker = (uint32_t [3]){1, 0, 0};
+    brackets = (int32_t [2]){1, 0};
     index = data->index;
     while (isspace((int)data->src[index]))
-    {
-        printf("Pass space\n");
         ++index;
-    }
     if (data->src[index] == ']')
-    {
-        printf("Pass square\n");
         return (0);
-    }
     len = 1;
-    while (data->src[index] != '\0' && checker[0] > 0)
+    while (data->src[index] != '\0' && brackets[0] > 0)
     {
-        printf("%c\n", data->src[index]);
-        if (data->src[index] == ',' && checker[0] == 1 && checker[1] == 0 && checker[2] == 0)
+        if (data->src[index] == ',' && brackets[0] == 1 && brackets[1] == 0 && quote == 0)
 			++len;
-		checker[0] += (data->src[index] == '[') - (data->src[index] == ']');
-		checker[1] += (data->src[index] == '{') - (data->src[index] == '}');
+		brackets[0] += (data->src[index] == '[') - (data->src[index] == ']');
+		brackets[1] += (data->src[index] == '{') - (data->src[index] == '}');
 		if (data->src[index] == '\"')
-			checker[2] ^= 1;
+			quote ^= 1;
         ++index;
     }
     return (len);
@@ -54,7 +48,6 @@ t_json_array *json_create_array(t_json_content *data)
     if ((arr = (t_json_array *)malloc(sizeof(t_json_array))) == NULL)
         return (NULL);
     arr->len = json_array_len(data);
-    printf("len = %d\n", arr->len);
     if ((arr->value = (t_json_value *)malloc(sizeof(t_json_value) * arr->len)) == NULL)
     {
         free(arr);
@@ -63,12 +56,9 @@ t_json_array *json_create_array(t_json_content *data)
     len = 0;
     while (len < arr->len)
     {
-        printf("len = %zu", len);
         arr->value[len] = *json_new_value(data);
         if (len < arr->len - 1)
             json_skip_comma(data);
-        printf("%p\n", &arr->value[len]);
-        //json_print_value(&arr->value[len]);
         ++len;
     }
     return (arr);
